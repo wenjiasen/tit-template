a9c72fbd6f83e3591985791cd8d7f3098aa6aee8
Jason <wenjsen@gmail.com>
2023-07-21 15:59:54 +0800

    更新配置示例

--- config/index.js
+++ config/index.js
@@ -1,4 +1,11 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   port: 8080,
+  mysql: {
+    host: "localhost",
+    port: "3306",
+    username: "YOUR USERNAME",
+    password: "YOUR PASSWORD",
+    database: "YOUR DATABASE NAME",
+  },
 };
